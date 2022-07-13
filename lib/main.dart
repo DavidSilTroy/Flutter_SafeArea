ba7cbfc0b9e45118c6fc2e1bad0c9811aa056456
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-07-13 10:44:15 +0200

    Showing how the main screen would look like using the safeArea

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter SafeArea',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text('Hola Mundo'),
+      home: const SafeArea(
+        child: Text('Hola Mundo'),
+      ),
     );
   }
 }
